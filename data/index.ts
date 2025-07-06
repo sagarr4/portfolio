24cc9382b8a23863437c12c943c5d08f153f2bc7
Sagar <sagarbmw1@gmail.com>
2025-07-06 16:17:12 -0700

    updated

---
+++ data/index.ts
@@ -0,0 +1,224 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize data driven decesions, fostering business growth.",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible working in any time zone",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently looking for new opportunities",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Smart Shift AI – AI-Powered Workforce Scheduling System (Time Series Forecasting)",
+    des: "Smart Shift AI uses time series forecasting to optimize workforce scheduling, improving efficiency and reducing labor costs.",
+    img: "/p1.svg",
+    iconLists: ["/Python.svg", "/sql.svg", "/docker.svg", "/tensorflow.svg", "/vscode.svg"],
+    link: "https://github.com/sagarr4/SmartShift",
+  },
+  {
+    id: 2,
+    title: "AI-Powered-Flight-Booking-Chatbot",
+    des: "An intelligent flight booking chatbot with both Web UI and Command Line Interface. Supports natural conversations, city name recognition, booking logic, and email confirmations.",
+    img: "/p2.svg",
+    iconLists: ["/Python.svg", "/sql.svg", "/docker.svg", "/tensorflow.svg", "/vscode.svg"],
+    link: "https://github.com/sagarr4/AI-Powered-Flight-Booking-Chatbot",
+  },
+  {
+    id: 3,
+    title: "Smart-Waste-Classification",
+    des: "The increasing volume of waste and ineffective manual sorting methods challenge sustainability goals. To address this, an AI-assisted waste sorting model is proposed to automate the classification of waste into recyclable, compostable, and garbage categories, enhancing recycling efficiency and supporting sustainable waste management.",
+    img: "/p3.svg",
+    iconLists: ["/Python.svg", "/sql.svg", "/docker.svg", "/tensorflow.svg", "/vscode.svg"],
+    link: "https://github.com/sagarr4/Smart-Waste-Classification-A-TensorFlow-and-Kera-s-Powered-Framework-Using-Advanced-Computer-Vision",
+  },
+  {
+    id: 4,
+    title: "Animal Species Recognition",
+    des: "This project applies advanced neural network architectures to enhance the accuracy of animal species recognition using image classification.",
+    img: "/p4.svg",
+    iconLists: ["/Python.svg", "/sql.svg", "/docker.svg", "/tensorflow.svg", "/vscode.svg"],
+    link: "https://github.com/sagarr4/CNN-Architectures-for-High-Accuracy-Animal-Species-Recognition-through-Advanced-TL-Techniques",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "Python",
+    img: "/Python.svg",
+    nameImg: "/Python.svg",
+  },
+  {
+    id: 2,
+    name: "TensorFlow",
+    img: "/tensorflow.svg",
+    nameImg: "/tensorflow.svg",
+  },
+  {
+    id: 3,
+    name: "SQL",
+    img: "/sql.svg",
+    nameImg: "/sql.svg",
+  },
+  {
+    id: 4,
+    name: "Docker",
+    img: "/docker.svg",
+    nameImg: "/docker.svg",
+  },
+  {
+    id: 5,
+    name: "VsCode.",
+   img: "/vscode.svg",
+    nameImg: "/vscode.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev - JSM Tech",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    url: "https://github.com/sagarr4",
+    alt: "GitHub",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    url: "https://twitter.com/yourusername",
+    alt: "Twitter",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    url: "https://linkedin.com/in/sagarrprajapati/",
+    alt: "LinkedIn",
+  },
+];
+
